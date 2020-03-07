e902159bc162846f8feefb08f35716a3079b1cd3
songxuecheng <songxuecheng@zingfront.com>
2020-03-07 17:16:06 +0800

    创建目录结构

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/7 下午5:15
+# @Author : songxuecheng
+# @Email : songxuecheng@zingfront.com
+# @File : __init__.py
+# @Project : practise
+# @description : 
---
+++ my_queue/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/7 下午5:14
+# @Author : songxuecheng
+# @Email : songxuecheng@zingfront.com
+# @File : __init__.py.py
+# @Project : practise
+# @description : 
